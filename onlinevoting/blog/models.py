54ed2fc9eb724e7ff540dd017290488d9e0cea38
Kushal <kushalnatani123@gmail.com>
2020-04-17 17:50:23 +0530

    till 6th tutorial with bugs

--- onlinevoting/blog/models.py
+++ onlinevoting/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Society(models.Model):
+    Name = models.CharField(max_length = 100)
+    Discription = models.TextField()
+    Admin = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.Name
 
 # Create your models here.
--- onlinevoting/blog/views.py
+++ onlinevoting/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Society
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+     'societies' : Society.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 def about(request):
     return render(request, 'blog/about.html')
