2530f2a989f00a04508b7f33eae3b40d913e36f1
iamCodeMonk <animeshbaruah7@gmail.com>
2020-04-21 22:21:33 +0530

    communication added

--- onlinevoting/blog/models.py
+++ onlinevoting/blog/models.py
@@ -6,12 +6,10 @@ class Society(models.Model):
     Name = models.CharField(max_length = 100)
     Discription = models.TextField()
     Admin = models.ForeignKey(User, on_delete = models.CASCADE)
-
     def __str__(self):
         return self.Name
 
     def get_absolute_url(self):
         return reverse('society-detail', kwargs={'pk':self.pk})
-
 
 # Create your models here.
--- onlinevoting/users/urls.py
+++ onlinevoting/users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views as users_views
+from queries import views as queries_views
+
 
 urlpatterns = [
     path('' , users_views.profile ,name = 'profile'),
     path('society/',  users_views.society, name = 'My Societies'),
     path('society/<int:pk>/' , users_views.SocietyDetailView.as_view() ,name = 'society-detail'),
-    path('society/new/', users_views.SocietyCreateView.as_view(), name = 'society_create')
+    path('society/new/', users_views.SocietyCreateView.as_view(), name = 'society_create'),
+    path('society/<int:pk>/<str:Name>/queries/', queries_views.PostListView.as_view(), name = 'queries-home'),
+    path('society/<int:pk>/<str:Name>/queries/new', queries_views.PostCreateView.as_view(), name = 'new-query'),
 ]
